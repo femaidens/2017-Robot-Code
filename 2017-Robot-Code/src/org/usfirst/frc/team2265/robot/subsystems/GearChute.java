813bcb6d7a609587eca47218a1ac0816625e4243
osullivl <osullivl@006-LT18.bxsci.78x445.nycboe.org>
2017-01-10 16:39:21 -0500

    Updated gear chute code (2 pistons)

--- 2017-Robot-Code/src/org/usfirst/frc/team2265/robot/subsystems/GearChute.java
+++ 2017-Robot-Code/src/org/usfirst/frc/team2265/robot/subsystems/GearChute.java
@@ -11,13 +11,16 @@ import edu.wpi.first.wpilibj.DoubleSolenoid;
 // makes gear chute subsystem
 //
 public class GearChute extends Subsystem {
-	public static DoubleSolenoid gearPiston = new DoubleSolenoid(RobotMap.gearport1, RobotMap.gearport2); 
+	public static DoubleSolenoid gearPiston = new DoubleSolenoid(RobotMap.gearport1, RobotMap.gearport2);
+	public static DoubleSolenoid gearPiston2 = new DoubleSolenoid (RobotMap.gearport3, RobotMap.gearport4);
 		public void extend() {
 		gearPiston.set(DoubleSolenoid.Value.kForward);
+		gearPiston2.set(DoubleSolenoid.Value.kForward);
 	}
     
 public void retract() {
 		gearPiston.set(DoubleSolenoid.Value.kReverse);
+		gearPiston2.set(DoubleSolenoid.Value.kReverse);
 	}
     // Put methods for controlling this subsystem
     // here. Call these from Commands.
