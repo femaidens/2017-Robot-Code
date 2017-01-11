0dfa7c58633cc63f8c05dd2cd9fc953c0aa61b70
carol shao <shaot@bxscience.edu>
2017-01-11 16:39:22 -0500

    updated comments

--- 2017-Robot-Code/src/org/usfirst/frc/team2265/robot/subsystems/GearChute.java
+++ 2017-Robot-Code/src/org/usfirst/frc/team2265/robot/subsystems/GearChute.java
@@ -1,34 +1,36 @@
 package org.usfirst.frc.team2265.robot.subsystems;
 
+//creates imports
 import org.usfirst.frc.team2265.robot.RobotMap;
 import edu.wpi.first.wpilibj.command.Subsystem;
 import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
 import edu.wpi.first.wpilibj.DoubleSolenoid;
 
 /**
- *
+ * 
  */
-// makes gear chute subsystem
-//
+// makes gear chute subsystem//
 public class GearChute extends Subsystem {
+
+//intializes doubleSolenoid for the gearChute
 	public static DoubleSolenoid gearPiston = new DoubleSolenoid(RobotMap.gearport1, RobotMap.gearport2);
-		
+	
+//Extends the piston
 	public static void extend() {
 		gearPiston.set(DoubleSolenoid.Value.kForward);
 	}
-    
+     
+//Retracts the piston
 	public static void retract() {
 		gearPiston.set(DoubleSolenoid.Value.kReverse);
 	}
+
     // Put methods for controlling this subsystem
     // here. Call these from Commands.
-
+	
 public void initDefaultCommand() {
     
         // Set the default command for a subsystem here.
         //setDefaultCommand(new MySpecialCommand());
     }
 }
-
-
-
