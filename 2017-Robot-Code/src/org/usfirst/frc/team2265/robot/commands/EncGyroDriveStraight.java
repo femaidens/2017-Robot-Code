628d479d0c89449c461209491604b75377d14101
FeMaidens <FeMaidens@100.104.26.120>
2017-02-01 17:13:43 -0500

    added autonomous code with smartdash options

---
+++ 2017-Robot-Code/src/org/usfirst/frc/team2265/robot/commands/EncGyroDriveStraight.java
@@ -0,0 +1,73 @@
+package org.usfirst.frc.team2265.robot.commands;
+
+import edu.wpi.first.wpilibj.AnalogGyro;
+import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+import org.usfirst.frc.team2265.robot.Robot;
+import org.usfirst.frc.team2265.robot.RobotMap;
+import org.usfirst.frc.team2265.robot.subsystems.Drivetrain;
+
+public class EncGyroDriveStraight extends Command {
+	double distance, left, right, distanceLeft, distanceRight;
+	AnalogGyro gyro;
+	double angle;
+	double leftVel = Drivetrain.frontRight.get();
+	double rightVel = Drivetrain.frontLeft.get();
+
+	public EncGyroDriveStraight(double d, double l, double r) {
+		// Use requires() here to declare subsystem dependencies
+		// eg. requires(chassis);
+		distance = d;
+		right = r;
+		left = l;
+		gyro = new AnalogGyro(RobotMap.gyroPort);
+		angle = gyro.getAngle();
+
+	}
+
+	// Called just before this Command runs the first time
+	protected void initialize() {
+		Drivetrain.encoderLeft.reset();
+		Drivetrain.encoderRight.reset();
+		gyro.reset();
+	}
+
+	// Called repeatedly when this Command is scheduled to run
+	protected void execute() {
+		if (angle > 90 && angle < 180) {// turning left
+			Drivetrain.frontRight.set(rightVel - 0.1);
+			Drivetrain.rearRight.set(rightVel - 0.1);
+		} else if (angle < 90 && angle > 0) {
+			Drivetrain.frontLeft.set(leftVel - 0.1);
+			Drivetrain.rearLeft.set(leftVel - 0.1);
+		}
+		distanceLeft = Drivetrain.encoderLeft.getDistance();
+		distanceRight = Drivetrain.encoderRight.getDistance();
+		System.out.println("Left Encoder Position" + distanceLeft);
+		System.out.println("Right Encoder Position" + distanceRight);
+		SmartDashboard.putNumber("Left Encoder Position: ", distanceLeft);
+		SmartDashboard.putNumber("Right Encoder Position: ", distanceRight);
+
+	}
+
+	// Make this return true when this Command no longer needs to run execute()
+	protected boolean isFinished() {
+		return Math.abs(distanceLeft) > distance || Math.abs(distanceRight) > distance;
+
+	}
+
+	// Called once after isFinished returns true
+	protected void end() {
+		Robot.drivetrain.drive(0, 0);
+		Drivetrain.encoderLeft.reset();
+		Drivetrain.encoderRight.reset();// remove if we want to see how far the
+										// encoder has moved AFTER stopping
+
+	}
+
+	// Called when another command which requires one or more of the same
+	// subsystems is scheduled to run
+	protected void interrupted() {
+	}
+}
---
+++ 2017-Robot-Code/src/org/usfirst/frc/team2265/robot/subsystems/GearChute.java
@@ -0,0 +1,55 @@
+package org.usfirst.frc.team2265.robot.subsystems;
+
+//creates imports
+import org.usfirst.frc.team2265.robot.RobotMap;
+import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.wpi.first.wpilibj.Servo;
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+
+/**
+ * 
+ */
+// makes gear chute subsystem//
+public class GearChute extends Subsystem {
+
+	// intializes doubleSolenoid for the gearChute
+	public static DoubleSolenoid gearPiston = new DoubleSolenoid(RobotMap.gearPort1, RobotMap.gearPort2);
+	public static Servo gearServo1 = new Servo(RobotMap.gearServoPort1);
+	public static Servo gearServo2 = new Servo(RobotMap.gearServoPort2);
+
+	public static void turnFlaps(boolean reset) {
+		if (reset) {
+			while (gearServo1.getAngle() < 90) {
+				gearServo1.set(0.5);
+				gearServo2.set(-0.5);
+			}
+			gearServo1.set(0.0);
+			gearServo2.set(0.0);
+		} else {
+			while (gearServo1.getAngle() < -90)
+				gearServo1.set(-0.5);
+			gearServo2.set(0.5);
+		}
+		gearServo1.set(0.0);
+		gearServo2.set(0.0);
+	}
+
+	// Extends the piston
+	public static void extend() {
+		gearPiston.set(DoubleSolenoid.Value.kForward);
+	}
+
+	// Retracts the piston
+	public static void retract() {
+		gearPiston.set(DoubleSolenoid.Value.kReverse);
+	}
+
+	// Put methods for controlling this subsystem
+	// here. Call these from Commands.
+
+	public void initDefaultCommand() {
+
+		// Set the default command for a subsystem here.
+		// setDefaultCommand(new MySpecialCommand());
+	}
+}
