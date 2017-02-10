5de3ae75b87c65c0b4991d9cb97443175e6faf90
FeMaidens <FeMaidens@100.104.26.18>
2017-02-10 16:46:41 -0500

    idk

--- 2017-Robot-Code/src/org/usfirst/frc/team2265/robot/subsystems/Drivetrain.java
+++ 2017-Robot-Code/src/org/usfirst/frc/team2265/robot/subsystems/Drivetrain.java
@@ -1,5 +1,4 @@
 
-
 package org.usfirst.frc.team2265.robot.subsystems;
 
 import edu.wpi.first.wpilibj.command.Subsystem;
@@ -29,8 +28,8 @@ public class Drivetrain extends Subsystem {
 	// Initialize solenoids
 	public static DoubleSolenoid gearShifter = new DoubleSolenoid(RobotMap.transIn, RobotMap.transOut);
 	public static RobotDrive tankDrive = new RobotDrive(frontLeft, rearLeft, frontRight, rearRight);
-	
-	//Initializing encoder 
+
+	// Initializing encoder
 	public static Encoder encoder = new Encoder(RobotMap.encPort1, RobotMap.encPort2);
 
 	public Drivetrain() {
@@ -41,7 +40,7 @@ public class Drivetrain extends Subsystem {
 		double leftVal = OI.driveJoystick.getRawAxis(1);
 		double rightVal = OI.driveJoystick.getRawAxis(5);
 		System.out.println("leftVal: " + leftVal + "rightVal: " + rightVal);
-		tankDrive.tankDrive(-leftVal * 0.6, -rightVal * 0.6);
+		tankDrive.tankDrive(-leftVal, -rightVal);
 	}
 
 	// auton
@@ -51,14 +50,16 @@ public class Drivetrain extends Subsystem {
 		frontLeft.set(l);
 		rearLeft.set(l);
 	}
-	//straight
-	public void driveStraight(){
+
+	// straight
+	public void driveStraight() {
 		double leftVal = OI.driveJoystick.getRawAxis(1);
 		double rightVal = OI.driveJoystick.getRawAxis(5);
-		double driveVal = (leftVal + rightVal)/2;
+		double driveVal = (leftVal + rightVal) / 2;
 		tankDrive.tankDrive(driveVal, driveVal);
-		
+
 	}
+
 	public void shiftToSpeed() {
 		gearShifter.set(DoubleSolenoid.Value.kReverse);
 	}
