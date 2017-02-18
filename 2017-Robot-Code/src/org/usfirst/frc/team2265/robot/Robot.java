d20b2b1ead089d81ff0c1a8d2241b30eb029dd0c
FeMaidens <FeMaidens@10.22.65.82>
2017-02-18 15:42:43 -0500

    distance calibration

--- 2017-Robot-Code/src/org/usfirst/frc/team2265/robot/Robot.java
+++ 2017-Robot-Code/src/org/usfirst/frc/team2265/robot/Robot.java
@@ -129,7 +129,7 @@ public class Robot extends IterativeRobot {
 		    			    		if(rectList.get(i).height> maxRect.height)
 		    			    			maxRect = rectList.get(i);
 		    			    	}
-		    			    	distance = -.0003502*(Math.pow(maxRect.height, 3)) +.082*(Math.pow(maxRect.height, 2)) - 7.01*maxRect.height+ 252.816;
+		    			    	distance = -.0003502*(Math.pow(maxRect.height, 3)) +.082*(Math.pow(maxRect.height, 2)) - 7.01*maxRect.height+ 253.816;
 		    			    	System.out.println("Distance from Peg: "+ distance);
 		    			    
 		    				}
